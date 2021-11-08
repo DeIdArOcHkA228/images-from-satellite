44636887f2f42361036a878379e7b335fe82fa85
DeIdArOcHkA228 <drozhkoolesya@gmail.com>
2021-11-08 12:02:55 +0300

    Добавил фукцию

--- main.py
+++ main.py
@@ -7,8 +7,13 @@ url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
 
 Path("images/").mkdir(parents=True, exist_ok=True)
 
-response = requests.get(url)
-response.raise_for_status()
 
-with open(filename, 'wb') as file:
-    file.write(response.content)
+
+
+
+def installing_pictures(picture_url, path):
+    response = requests.get(picture_url)
+    response.raise_for_status()
+    with open(path, 'wb') as file:
+        file.write(response.content)
+test = installing_pictures(url, filename)
