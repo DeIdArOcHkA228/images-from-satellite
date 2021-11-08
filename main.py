dd4251b2e151ed9104dd0e6947859205241bd071
DeIdArOcHkA228 <drozhkoolesya@gmail.com>
2021-11-08 11:29:48 +0300

    Сделал сохранение картинки в папку

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import requests
+from pathlib import Path
 
 
-filename = 'satellite.jpeg'
+filename = 'images/satellite.jpeg'
 url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
+
+Path("images/").mkdir(parents=True, exist_ok=True)
 
 response = requests.get(url)
 response.raise_for_status()
